edf1226470d1c43a884bee65f69761d5a3d97887
d-grab <danielgrabinski@yahoo.com>
2023-08-02 17:33:55 +0100

    Loops

---
+++ Loops.java
@@ -0,0 +1,59 @@
+public class Loops {
+    public static void main(String[] args) {
+
+
+    // int numbers[] = {1,2,3,4,5};
+
+        // int sum=0;
+        // //Sum of numbers
+        // for(int number : numbers) {
+
+        //     sum+=number;   
+        // System.out.println(sum);
+
+
+        // //Printing odd number
+        // for(int number = 1; number <=50; number++){
+        //     if(number% 2 == 1){
+        //         System.out.println(number);
+        //     }
+        // }
+
+
+
+
+    //Multiplication table 
+    // for(int number =1; number<=10; number ++){
+    //     for(int multiplier =1;multiplier<=10;multiplier++){
+    //         System.out.printf("%d X %d = %d \n", number, multiplier,number*multiplier);
+    //     }
+    // }
+
+
+
+    // //Multiplier of number 5
+    //     int number = 5;
+
+    // for (int multiplier =1; multiplier< 10; multiplier++){
+    //     System.out.printf("%d X %d = %d \n", number,multiplier,number*multiplier);
+    // }
+
+
+
+
+    //     int numbers[] = {1,2,3,4,5,6,7,8,9,10} ;
+    //     int sum=0;
+
+    //     // Summing the numbers of the array
+    //     for (int index=0; index < numbers.length; index++) {
+    //         sum+= numbers[index];
+         
+    //  }  System.out.println(sum);   
+
+
+    //     //Create for loop  to print number
+    // for (int number=1; number <=10; number++) {
+    //     System.out.println(number);
+    // }
+
+}}
--- Main.java
+++ Main.java
@@ -1,60 +1,7 @@
-import java.util.Arrays;
+
 
  class Main {
  
     public static void main(String[] args) {
-    
-
-        int numbers[] = {1,2,3,4,5};
-        //Lenght of the same array
-        int copyofNumbers[]=Arrays.copyOf(numbers, numbers.length);
-        //Lenght specified 
-        //int copyofNumbers1[]=Arrays.copyOf(numbers, 10);
-
-        //Replacing elements of the Array
-        //Arrays.fill(numbers, 0);
-        System.out.println(Arrays.toString(numbers));
-        System.out.println(Arrays.toString(copyofNumbers));
-
-        //Check if the arrays are the same
-        System.out.println(Arrays.equals(numbers, copyofNumbers));
-
-
-
-        //char vowels[] = new char[5];
-        // char vowelsNew[] = {'e','u','a','o','i'};
-
-        // Filling array with values
-       // Arrays.fill(vowelsNew, 'x');
-       // System.out.print(vowelsNew);
-
-        //Filling array with values in certain indexes
-        // int start = 1;
-        // int end = 2;
-        // Arrays.fill(vowelsNew,1,2, 'x');
-        // System.out.print(Arrays.toString(vowelsNew));
-
-        // char key ='o';
-        // Arrays.sort(vowelsNew);
-
-        // //findin index of looking element
-        // int foundItemIndex = Arrays.binarySearch(vowelsNew, key);
-
-
-        // // // Print Array
-        //  System.out.println(Arrays.toString(vowelsNew));
         
-        // // Print looking letter in the array
-        // System.out.printf("The looking letter is - %c -", vowelsNew[foundItemIndex]);
-        // //Lenght of array
-        // System.out.println(vowelsNew.length);
-  
-    }
-}
-
-  // // a e i o u
-    //     vowels[0] = 'a';
-    //     vowels[1] = 'e';
-    //     vowels[2] = 'u';
-    //     vowels[3] = 'o';
-    //     vowels[4] = 'u';
+    }}
