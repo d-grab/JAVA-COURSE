ecb43338cb7b13ac38f72d275b6fa7a4cb2764d6
d-grab <danielgrabinski@yahoo.com>
2023-08-02 13:49:15 +0100

    Scanner Method

--- Main.java
+++ Main.java
@@ -17,5 +17,12 @@ class Main {
     //SCANNER INPUT
 
     Scanner scanner = new Scanner (System.in);
+        
+        System.out.println("What is your name");
+        String name = scanner.nextLine();
+
+        System.out.println("My name is " + name);
+
+        scanner.close();
     }
 }
