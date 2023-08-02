216ef296033aeae397e6a9648dfac36897467dbd
d-grab <danielgrabinski@yahoo.com>
2023-08-02 13:44:01 +0100

    Java course

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+ static int age;
+    public static void main(String[] args) {
+        String name = new String ("Daniel");
+        String country = new String ("daniel");
+
+        String formattedString = String.format("My name is %s I am from %s",name,country);
+
+        String newName= name.replace("Daniel", "Rambo");
+       System.out.println(formattedString);
+       System.out.println(name.equalsIgnoreCase(country));
+       System.out.println(newName);
+       System.out.println(formattedString.contains("My name"));
+    }
+}
