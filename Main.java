21e3d988b566d93699035bc0028254528e0fcf49
d-grab <danielgrabinski@yahoo.com>
2023-08-02 19:56:14 +0100

    OOP and inheritants

--- Main.java
+++ Main.java
@@ -1,7 +1,23 @@
-import java.util.HashMap;
+import java.time.LocalDate;
 
 class Main {
-    public static void main(String[] args) {   
-      ;
+    public static void main(String[] args) {  
+  //initialising object User 
+      
+      
+
+    //   User user = new User("Daniel Grabinski","1994-09-20");
+
+      Book Father = new Book("Rich Father","Robert Kiosaki", 270);  
+      AudioBook sleep = new AudioBook("Earn when You sleep", "Sleeper",30000);
+      Ebook richer = new Ebook("Getting reach", "Adam Browley", 280, "pdf");
+
+      System.out.println(Father.toString());
+      System.out.println(sleep.toString());
+      System.out.println(richer.toString());
+    // System.out.printf("%s was born in %s and is %d years old \n",user.getName(),user.getBirthDate(),user.age());
+    // System.out.printf("%s Has borrowed these books: %s",user.getName(), user.borrowedBooks());
+  
+
         
     }}
---
+++ User.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+
+public class User {
+    private String name;
+    private LocalDate birthDate;
+    private ArrayList<Book> books = new ArrayList<Book>();
+
+    //Constructor
+    User(String name, String birthDate) {
+        this.name = name;
+        this.birthDate = LocalDate.parse(birthDate);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getBirthDate() {
+        return this.birthDate.toString();
+    }
+
+    public void borrow(Book book) {
+        this.books.add(book);
+    }
+
+    public String borrowedBooks() {
+        return this.books.toString();
+    }
+    
+    public int age() {
+        int age = Period.between(this.birthDate, LocalDate.now()).getYears(); 
+
+        return age;
+    }
+}
